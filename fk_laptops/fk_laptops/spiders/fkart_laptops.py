3230c3b8325bc30f665da2373b9b3584a3077781
19981999ab <19981999ab@gmail.com>
2019-06-02 17:38:30 +0530

    Add files via upload

--- fk_laptops/fk_laptops/spiders/fkart_laptops.py
+++ fk_laptops/fk_laptops/spiders/fkart_laptops.py
@@ -12,17 +12,14 @@ class FkartLaptopsSpider(scrapy.Spider):
         '=AS_QueryStore_OrganicAutoSuggest_0_6&otracker1=AS_QueryStore_OrganicAutoSuggest_0_6&as-pos=0&as-type=RECENT'
         '&as-searchtext=loptop&page=1']
     no_of_laptops = sys.argv[1]  ##total number of laptops to be scrapped
-    no_of_laptops = eval(no_of_laptops) ###to convert to integer
+    no_of_laptops = eval(no_of_laptops)
     no_of_loops = no_of_laptops // 24  ##there are 24 laptops in 1 page
     no_of_items_in_loop = no_of_laptops % 24
-    loop_count = 0 
+    loop_count = 0
     items_count=0
-    all_laptop_details=[]  ###List containing the details of laptop stored in form of dictionaries 
+    all_laptop_details=[]
     max_page_number=30 ##30 pages in laptop category
-'''
-Since there are 24 laptops in 1 page, we divide the total no. of laptops by 24, the integer part (loop_count) of the result will tell us how many time 
-the loop will run (each loop corresponds to 24 laptops) and the remainder (%24) (items_count) gives the no. of times the last loop will run.
-'''
+
     pickle_directory=sys.argv[2]
 
     def parse(self, response):
